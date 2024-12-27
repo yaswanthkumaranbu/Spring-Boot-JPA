4b06546ddf8a5e06bd25c124017985a57a0a6541
yaswanthkumaranbu <yaswanthkumaranbu@gmail.com>
2024-12-27 11:51:53 +0530

    JPA h2 database

---
+++ demo/src/main/java/com/example/demo/dao/AlienRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Alien;
+
+public interface AlienRepo extends CrudRepository<Alien, Integer> {
+
+}
--- demo/src/main/java/com/example/demo/model/Alien.java
+++ demo/src/main/java/com/example/demo/model/Alien.java
@@ -1,6 +1,12 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Alien {
+	
+	@Id
 	private int aid;
 	private String aname;
 	public int getAid() {
